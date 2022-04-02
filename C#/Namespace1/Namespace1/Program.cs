9229dde095efee9449ffc348787658deb2d4701a
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-04-02 13:04:50 +0530

    first

---
+++ C#/Namespace1/Namespace1/Program.cs
@@ -0,0 +1,36 @@
+﻿/* using system is Namespace
+
+A namespace is used to organize your code and is collection of
+classes
+interface
+struct
+enum
+delegate
+
+>>A Namespace contails other Namespaces also
+
+
+>> Main method is the entry point to the program......
+*/
+
+
+
+
+
+using System; 
+
+
+    internal class Program
+    {
+        static void Main1()
+        {
+            Console.WriteLine("Hello World! from main 1 method.");
+        }
+
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        Main1();
+    }
+}
+
---
+++ C#/ReadAndWrite/ReadAndWrite/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //write operation
+            Console.WriteLine("Hello dear please mention your Name: ");
+
+        // read operation
+        var name=Console.ReadLine();
+
+        //concat operation
+        Console.WriteLine("welcome dear " + name);
+
+        //placeHolder operation
+        Console.WriteLine("welcome dear {0}", name);
+
+
+        }
+    }
+
