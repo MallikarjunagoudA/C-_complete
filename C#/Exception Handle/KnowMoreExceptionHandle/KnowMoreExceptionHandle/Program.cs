b7186342605d659b570611815d711b251713f2c7
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-05-09 17:31:36 +0530

    exception Handle

---
+++ C#/Exception Handle/KnowMoreExceptionHandle/KnowMoreExceptionHandle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace KnowMoreExceptionHandle
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(@"E:\Kudvenkat tutorial\C#\Delegates\readMEe.txt");
+                Console.WriteLine(sr.ReadToEnd());
+                sr.Close();
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception exx)
+            {
+                Console.WriteLine(exx.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("something went wrong please try again");
+                //sr.Close();
+
+            }
+
+        }
+    }
+}
